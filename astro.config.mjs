3ec2c61490aadb8834f619c2de744be9a1e677d8
Gogh NT <gogh.nt@gmail.com>
2025-05-05 18:08:44 +0300

    UPDATE ICON

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: "https://www.salem-des.com/",
-  output: "static",
+  output: "server",
   adapter: vercel(),
   integrations: [react(), sitemap()],
   vite: {
