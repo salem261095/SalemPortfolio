7cd9808d67b6afd327836578d2fbaccee95b481c
Gogh NT <gogh.nt@gmail.com>
2025-05-05 15:01:13 +0300

    Update testimonials slider with smooth transition

--- src/pages/api/testimonials.ts
+++
@@ -1,8 +0,0 @@
-import { getTestimonials } from "@/graphql/fetcher/fetchTestimonials";
-
-export async function GET(){
-    const Testimonials = await getTestimonials();
-return new Response(JSON.stringify(Testimonials), {
-    headers:{"Content-Type": "application/json"}
-});
-}
